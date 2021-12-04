aa891dd0e005383d969448a51608a9971d3b0282
Nick Reilingh <nreilingh@gmail.com>
2021-12-04 17:12:44 -0500

    day 3 pt 1 (finished yesterday)

--- 2021/rs/src/main.rs
+++ 2021/rs/src/main.rs
@@ -4,9 +4,11 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     match args[1].as_str() {
         "1" => depth_measurements(),
         "2" => sub_position(),
+        "3" => binary_diagnostic(),
         _ => (),
     }
 }
@@ -115,4 +117,56 @@ fn sub_position() {
              naive_position.depth * naive_position.distance);
     println!("The position vector product is {}",
              position.depth * position.distance);
+}
+
+// Day three
+fn binary_diagnostic() {
+    let diagnostics: Vec<String> = read_lines("../inputs/03.txt");
+
+    let meas_len = diagnostics[0].chars().count();
+    let meas_count = diagnostics.len();
+
+    // The plan:
+    // Measure the input to get the number of inputs and the number of digits
+    // Allocate an int for each position and count the number of trues in each position
+    // Subtract from the total number of inputs to get the falses;
+    // basically just determine if it is less than or more than half the inputs
+    // Then reduce this into two strings of binary and then parse them as such
+
+    let diag_digits = diagnostics.iter().fold(vec![0; meas_len], |mut acc, item| {
+        for (i, char) in item.chars().enumerate() {
+            acc[i] += char.to_digit(10).unwrap();
+        }
+        acc
+    });
+    println!("Diag digits are {:?} and meas count is {}", diag_digits, meas_count);
+
+    let bits = diag_digits.iter().fold((String::from(""), String::from("")), |mut acc, item| {
+        if item * 2 > meas_count.try_into().unwrap() {
+            acc.0.push_str("1");
+            acc.1.push_str("0");
+        } else {
+            acc.0.push_str("0");
+            acc.1.push_str("1");
+        }
+        acc
+    });
+    println!("Bits are {:?}", bits);
+
+    struct Metrics {
+        gamma: u32,
+        epsilon: u32,
+    }
+    impl Metrics {
+        fn power_consumption(&self) -> u32 {
+            self.gamma * self.epsilon
+        }
+    }
+    let rate = Metrics {
+        gamma: u32::from_str_radix(&bits.0, 2).unwrap(),
+        epsilon: u32::from_str_radix(&bits.1, 2).unwrap(),
+    };
+    println!("Submarine metrics are {} gamma, {} epsilon", rate.gamma, rate.epsilon);
+
+    println!("submarine power consumption is {}", rate.power_consumption());
 }
